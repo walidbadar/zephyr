0fdabf833cb2fd3fe0ecc1c76ac2ff76368aceb2
Muhammad Waleed Badar <walid.badar@gmail.com>
2026-06-28 00:44:37 +0500

    soc: qca: add qualcomm atheros qca9531 soc support
    Signed-off-by: Muhammad Waleed Badar <walid.badar@gmail.com>

---
+++ soc/qca/qca9531/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Muhammad Waleed Badar
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __QCA_QCA9531_SOC_H__
+#define __QCA_QCA9531_SOC_H__
+
+#define MIPS_MACHINE_TIMER_IRQ      7  /* Machine Timer Interrupt */
+
+#endif /* __QCA_QCA9531_SOC_H__ */
